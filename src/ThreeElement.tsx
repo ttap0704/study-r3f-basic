760236881514372cb808c6a0ffd8201777c2614f
leedg0704 <leedg0704@gmail.com>
2024-02-14 22:03:43 +0900

    useFrame 학습

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,10 +1,22 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
 
 function ThreeElement() {
+  const el = useRef<THREE.Mesh>(null);
+
+  useFrame(() => {
+    if (el.current) {
+      el.current.rotation.x += 0.01;
+    }
+    // console.log(el.current);
+  });
+
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
       <mesh
+        ref={el}
         rotation={[
           THREE.MathUtils.degToRad(45),
           THREE.MathUtils.degToRad(45),
